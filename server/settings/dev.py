a9424f6f548bc930346fedf291806a6f69cb5c64
Rushikesh Pupale <pupalerushikesh@gmail.com>
2018-05-20 12:21:50 +0530

    Added settings module

---
+++ server/settings/dev.py
@@ -0,0 +1,3 @@
+from server.settings.base import *
+
+#override settings here
---
+++ server/settings/local.py
@@ -0,0 +1,9 @@
+#import sys
+import os
+
+
+
+
+
+os.environ['SECRET_KEY'] = '%g=_!9t)x1&n+bez*tio63^wd8k&_xq0za$98#!590cq7jhmks'
+os.environ['APIKey'] = '1234567890123456789euwnbjdbcj'
