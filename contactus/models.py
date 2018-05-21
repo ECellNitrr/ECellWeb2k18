74863d96705ddd899a33cbf2919ec82f637dca6f
BadduCoder <sumit7engineer@gmail.com>
2018-05-21 11:05:14 +0530

    Removed default value from user in message

--- contactus/models.py
+++ contactus/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from appprofile.models import Profile
 
 class Message(models.Model):
-	user = models.ForeignKey(Profile,on_delete=models.CASCADE,default=4)
+	user = models.ForeignKey(Profile,on_delete=models.CASCADE)
 	subject = models.CharField(max_length=256)
 	message = models.TextField()
 	flag = models.BooleanField()
