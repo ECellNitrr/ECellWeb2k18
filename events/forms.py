6fa6b5b830810a9b57a8621e80989967acc58e34
Rushikesh Pupale <pupalerushikesh@gmail.com>
2018-06-02 17:44:34 +0530

    added events api

--- events/forms.py
+++ events/forms.py
@@ -8,7 +8,9 @@ class EventForm(forms.ModelForm):
     cover_pic = forms.ImageField()
     icon = forms.ImageField()
     email = forms.EmailField()
+    date = forms.DateField()
+    time = forms.CharField()
 
     class Meta():
         model = Event
-        fields = ('name','venue', 'details', 'cover_pic', 'icon','email')
+        fields = ('name','venue', 'details', 'cover_pic', 'icon','email','date','time')
--- events/urls.py
+++ events/urls.py
@@ -4,5 +4,11 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    path('list/',views.get_events, name='Get_Event'),
+    path('list/',views.get_event, name='Get_Event'),
+    url(r'^add/', views.add_event),
+    url(r'^edit/(?P<pk>[-\w]+)/$',views.edit_event),
+    url(r'^(?P<pk>[-\w]+)/$',views.event_detail, name='detail'),
+    url(r'^delete/(?P<pk>[-\w]+)/$',views.delete_event, name='delete'),
+    
+
 ]
