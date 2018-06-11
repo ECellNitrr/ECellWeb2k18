278ff7afa360a911bccf901af7217f0f10db99ed
Rushikesh Pupale <pupalerushikesh@gmail.com>
2018-06-11 22:25:29 +0530

    Update .env file

--- server/settings.py
+++ server/settings.py
@@ -142,7 +142,7 @@ SITE_HEADER = os.environ.get('SITE_HEADER', 'Entrepreneurship Cell NIT Raipur')
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'noreplyecellnitrr@gmail.com'
-EMAIL_HOST_PASSWORD = 'Ecell2k18'
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 EMAIL_PORT = 587
