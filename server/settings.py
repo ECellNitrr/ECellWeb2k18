c67a9ca45f1b63e6605fdbe3ce558ac54a822d2b
Rushikesh Pupale <pupalerushikesh@gmail.com>
2018-07-07 10:58:50 +0530

    Added cors header

--- server/settings.py
+++ server/settings.py
@@ -54,9 +54,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'social_django',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware'
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -86,6 +88,8 @@ TEMPLATES = [
         },
     },
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 WSGI_APPLICATION = 'server.wsgi.application'
 
