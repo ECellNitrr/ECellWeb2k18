60ed37d76b7268309eaf5c806672e38cb1685d54
naveennvrgup <naveennvrgup@gmail.com>
2018-07-25 20:48:49 +0530

    speaker edit nearly done

--- static/website/speakers/speakers.js
+++ static/website/speakers/speakers.js
@@ -1,33 +1,41 @@
 reverse = false
 
-function insert_speaker(speakers, location) {
+function insert_speaker(speakers, location, to_hide) {
     var year = speakers[0].year
     // create a div
     var div = document.createElement('div')
     div.id = year
-    div.classList.add('years')
-    div.innerHTML += `<h3 class='display-4 font-weight-bold'>${year}</h3><hr>`
-
+    div.classList.add('years', to_hide ? "d-none" : "d-block")
+    // div.innerHTML += `<h3 class='display-4 font-weight-bold'>${year}</h3><hr>`
+    // make the template
     speakers.forEach(speaker => {
-        div.innerHTML += 
-            `<div class='d-flex speaker container ${reverse? "reverse":""}'>
-                    <div class='s-img'>
+        div.innerHTML += reverse?`
+            <svg  class=svg-top xmlns=http://www.w3.org/2000/svg viewBox="0 0 100 10" preserveAspectRatio=none>
+                <polygon points="100 0 100 10 0 10" /> </svg>
+        `:''
+        div.innerHTML +=
+            `<div class=${reverse? 'grey':'mt-4'}>
+                <div class='d-flex speaker container ${reverse ? "reverse pb-4" : ""}'>
+                    <div class='s-img text-left'>
                         <img src='/${speaker.profile_pic}' class="wow flipInX" alt="">
                     </div>
                     <div class='s-info d-flex align-items-center'>
                         <div class="wow fadeInUp">
                             <h4>${speaker.name}</h4>
                             <h5>${speaker.company}</h5>
+                            <p>Year of speech: ${year}</p>                            
                             <p>${speaker.description}</p>
                         </div>
                     </div>
-            </div>`
+            </div></div>`
+        div.innerHTML +=reverse?`
+            <svg class=svg-bottom xmlns=http://www.w3.org/2000/svg viewBox="0 0 100 10" preserveAspectRatio=none>
+                <polygon points="100 0 100 10 0 10" /> </svg>
+        `:''
 
-        reverse = !reverse 
+        reverse = !reverse
     })
 
-    // append div to the dom
-    
     location.append(div)
 }
 
@@ -40,11 +48,19 @@ $(function () {
         var k16 = data.speakers.filter(speaker => speaker.year == 2016)
         var k15 = data.speakers.filter(speaker => speaker.year == 2015)
 
-        insert_speaker(k18, speakers_div)
-        insert_speaker(k17, speakers_div)
-        insert_speaker(k16, speakers_div)
-        insert_speaker(k15, speakers_div)
+        insert_speaker(k18, speakers_div, false)
+        insert_speaker(k17, speakers_div, true)
+        insert_speaker(k16, speakers_div, true)
+        insert_speaker(k15, speakers_div, true)
 
-        hideOldYears()
     })
+})
+
+// load more button handler
+
+$('#load_more_btn').on('click', function() {
+    document.querySelector('.d-none').classList.remove('d-none')
+    if(!document.querySelector('.d-none')){
+        $(this).hide()
+    }
 })
