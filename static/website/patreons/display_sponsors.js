ac1431e3a3c0b1492c884b65354f6f199804b65f
naveennvrgup <naveennvrgup@gmail.com>
2018-07-28 13:00:22 +0530

    sponsors page fixed

--- static/website/patreons/display_sponsors.js
+++ static/website/patreons/display_sponsors.js
@@ -1,7 +1,7 @@
 let sponsors_html = document.querySelector('.patreons');
 let sponsors = null;
 
-let type= {
+let type = {
     'AS': 'Associate Sponsors',
     'PLTS': 'Platinum Sponsors',
     'GS': 'Gold Sponsors',
@@ -11,21 +11,21 @@ let type= {
 
 $(document).ready(function () {
     $.get('https://ecellnitrr.herokuapp.com/sponsor/list/').done(function (data) {
-        sponsors = data.Sponsor_List;
-        // console.log(JSON.stringify(sponsors, null, 2));
-
-        sponsors.forEach(function (sponsor) {
-            // console.log(sponsor);
-            sponsors_html.innerHTML += ` 
-                <div class='sponsor'>
-                    <img src='https://ecellnitrr.herokuapp.com/${sponsor.pic}'>
-                    <div class='details'>
-                        <h2>${sponsor.name}</h2>
-                        <p class='body'>${sponsor.details}</p>
-                        <p class='contact_no'>${sponsor.contact}</p>
-                        <p class='website'><a href='${sponsor.website}'>website</a></p>
-                    </div>
-                </div>`;
+        sponsors = data.spons;
+        sponsors.forEach(function (type) {
+            type.sponsors.forEach(function (sponsor) {
+                console.log(JSON.stringify(sponsor, null, 2));
+                sponsors_html.innerHTML += ` 
+                    <div class='sponsor'>
+                        <img src='https://ecellnitrr.herokuapp.com/${sponsor.pic}'>
+                        <div class='details'>
+                            <h2>${sponsor.name}</h2>
+                            <p class='body'>${sponsor.details}</p>
+                            <p class='contact_no'>${sponsor.contact}</p>
+                            <p class='website'><a href='${sponsor.website}'>website</a></p>
+                        </div>
+                    </div>`;
+            })
         })
     })
 })
