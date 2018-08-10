97f3f6cd0bbdc4071f931eea68fdfc85de641dc8
aditya999123 <aditya.agrawal@fyle.in>
2018-08-10 23:18:02 +0530

    nginx changes complete | ssl remains

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 source envars.sh
 
-cp ecellnginx /etc/nginx/sites-available
+sudo cp ecellnginx /etc/nginx/sites-available
 sudo service nginx restart
 
 gunicorn --bind 0.0.0.0:8000 server.wsgi:application --workers 3
