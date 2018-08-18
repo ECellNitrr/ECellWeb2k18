1ce93f3a989065aba6861bfc0af2fab3c53e9069
naveennvrgup <naveennvrgup@gmail.com>
2018-08-18 10:15:41 +0530

    styled the event_detail page

--- static/website/event_detail/event_detail.js
+++ static/website/event_detail/event_detail.js
@@ -1,38 +1,40 @@
-var events = document.querySelector('#articles')
+var event_detail = document.querySelector('#event_detail')
+console.log(event_detail)
 
-put_events_in_place = (data) => {
-    
-    
-    events.innerHTML += (`
-            <article>
-                <div class=article-layout>
-                    <div class=text>
-                        <h2 class=""><a href="/event_detail/${event.id}">${event.name}</a></h2>
-                        <p><strong>Venue: </strong>${event.venue} on ${event.date} at ${event.time}</p>
-                        <p>${event.details}</p>
-                        <div class='center_btn'>
-                            <button class="register_btn" data-eid=${event.id}>Register</button>
-                        </div>
+put_event_in_place = (event) => {
+    event_detail.innerHTML += (`
+            <article class="container">
+                <h2 class="">${event.name}</h2>
+                <hr>
+                <div class="img ">
+                    <img src='${event.cover_pic}' alt=""> </div>
+                </div>
+                <div class=text>
+                    <p><strong>Date: </strong>${event.date}</p>
+                    <p><strong>Time: </strong>${event.time}</p>
+                    <p><strong>Email: </strong>${event.email}</p>
+                    <p><strong>Venue: </strong>${event.venue} on ${event.date} at ${event.time}</p>
+                    <p class="text-justify">${event.details}</p>
+                    <div class='text-center mt-5'>
+                        <button class="register_btn" data-eid=${event.id}>Register</button>
                     </div>
-                    <div class="img ">
-                        <img src='${event.cover_pic}' alt=""> </div>
                 </div>
             </article>
         `)
 }
 
-$(function () {
-    $.get('/event/list/').then(function (data) {
-        console.log(data)
-        // hide the spinner
-        $('#spinner').hide(1000);
-        // put data
-        put_events_in_place(data)
-        // to add event listener
-        register_stuff(data)
-    })
-})
+fetch('/event/list')
+    .then(data => data.json())
+    .then((data) => {
+        // close the spinner
+        document.querySelector('#spinner').remove()
 
-// document.querySelector('#spinner').remove();
-// put_events_in_place(data)
-// register_stuff(data)
+        // put the data into place
+        var page_eid = window.location.href.split('/').pop()
+        var event = data.Events.filter((event) => event.id == page_eid)[0]
+        put_event_in_place(event)
+        console.log(JSON.stringify(event, null, 2))
+
+    })
+    .catch(err => console.error(err))
+
