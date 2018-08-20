cebdebbb57afc567b6a6a5adacf86c219cef826c
naveennvrgup <naveennvrgup@gmail.com>
2018-08-20 12:16:54 +0530

    basic click works gallery

--- static/website/gallery/gallery.js
+++ static/website/gallery/gallery.js
@@ -1,9 +1,39 @@
+// show the gallery
 var gallery_div = document.querySelector('#gallery')
-
-for(var i=0; i<12;i++){
-    gallery_div.innerHTML+= `
+for (var i = 0; i < 12; i++) {
+    gallery_div.innerHTML += `
         <div class='gimg'>
-            <img src='/static/website/gallery/img/${i+1}.jpg'>
+            <img src='/static/website/gallery/img/${i + 1}.jpg'>
         </div>
     `
 }
+
+// on click show big img
+var gimgs = document.querySelectorAll('.gimg img')
+var bimg = document.querySelector('#big_img')
+var bcontainer = document.querySelector('#big_container')
+var bcloseBtn = document.querySelector('#bcloseBtn')
+
+gimgs.forEach(gimg => {
+    gimg.addEventListener('click', function (e) {
+        e.preventDefault()
+        bimg.src = gimg.src
+        bcontainer.style.top = 0
+        bimg.style.height = '90%'
+    })
+})
+
+
+bcontainer.addEventListener('click', (e) => {
+    e.preventDefault()
+    // if clicked on img return 
+    if (e.target != bcontainer) return
+    // if clicked on the container
+    bcontainer.style.top = "-100vh"
+    bimg.style.height = '0%'
+})
+
+bcloseBtn.addEventListener('click', (e) => {
+    e.preventDefault()
+    console.log('bcloseBtn')
+})
