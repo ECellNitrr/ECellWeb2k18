185522e6ac29dbbd68f56687e79be9b46bf26f44
naveennvrgup <naveennvrgup@gmail.com>
2018-08-20 11:31:45 +0530

    images selected

--- static/website/gallery/gallery.js
+++ static/website/gallery/gallery.js
@@ -1,1 +1,9 @@
-alert('gallery')
+var gallery_div = document.querySelector('#gallery')
+
+for(var i=0; i<12;i++){
+    gallery_div.innerHTML+= `
+        <div class='gimg'>
+            <img src='/static/website/gallery/img/${i+1}.jpg'>
+        </div>
+    `
+}
