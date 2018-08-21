1363264eb4cdf3e2c9c0b5f110c9c43986b905dd
naveennvrgup <naveennvrgup@gmail.com>
2018-08-22 00:53:54 +0530

    mentors page years js done

--- static/website/mentors/mentors.js
+++ static/website/mentors/mentors.js
@@ -1,20 +1,40 @@
-$(function () {
-    $.get(base_url + '/mentors/list/').done(function (data) {
-        var mentors = $('.people');
-        console.log(data)
-        $('#spinner').hide();
-        
-        data.mentors.forEach(function (mentor) {
-            console.log(mentor);
-            mentors.append(`
-                <div>
-                    <img class="" src='${mentor.profile_pic}' alt="">
-                        <div class="">
-                        <h3>${mentor.name}</h3>
-                        <h4>${mentor.description}</h4>
-                    </div>
-                </div>
-            `)
-        })
+var this_mentors = document.querySelector('#this_year .people')
+var prev_mentors = document.querySelector('#prev_year .people')
+var this_div = document.querySelector('#this_year')
+var prev_div = document.querySelector('#prev_year')
+
+fetch(base_url + '/mentors/list/')
+    .then(data => data.json())
+    .then(function (data) {
+        document.querySelector('#spinner').remove();
+        var mentors = data.mentors
+        var this_data = mentors.filter(ele => ele.year == 2018)
+        var prev_data = mentors.filter(ele => ele.year != 2018)
+
+        insert_mentors(this_data, this_mentors)
+        insert_mentors(prev_data, prev_mentors)
+
+        if (this_data.length) this_div.style.display = 'block'
+        if (prev_data.length) prev_div.style.display = 'block'
+
+        console.log(JSON.stringify(this_data, null, 2))
+        console.log(JSON.stringify(prev_data, null, 2))
+
+
     })
-})
+
+
+insert_mentors = (mentors, location) => {
+    mentors.forEach(function (mentor) {
+        console.log(mentor);
+        location.innerHTML += (`
+            <div>
+                <img class="" src='${mentor.profile_pic}' alt="">
+                    <div class="">
+                    <h3>${mentor.name}</h3>
+                    <h4>${mentor.detail}</h4>
+                </div>
+            </div>
+        `)
+    })
+}
