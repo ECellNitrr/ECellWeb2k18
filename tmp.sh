6d89e3606f2d76020ba84cc97557066d00d3aabe
aditya999123 <aditya.agrawal@fyle.in>
2018-08-21 17:59:46 +0530

    tmp and ngrol removed

--- tmp.sh
+++
@@ -1,4 +0,0 @@
-mkdir /etc/systemd/system/nginx.service.d
-printf "[Service]\nExecStartPost=/bin/sleep 0.1\n" > /etc/systemd/system/nginx.service.d/override.conf
-systemctl daemon-reload
-systemctl restart nginx 
