571bcacb204bd3642e68e9062cfc1d6220258b50
naveennvrgup <naveennvrgup@gmail.com>
2018-08-22 14:48:27 +0530

    fail case added for mentors and events page

--- static/website/mentors/mentors.js
+++ static/website/mentors/mentors.js
@@ -7,20 +7,24 @@ fetch(base_url + '/mentors/list/')
     .then(data => data.json())
     .then(function (data) {
         document.querySelector('#spinner').remove();
-        var mentors = data.mentors
-        var this_data = mentors.filter(ele => ele.year == 2018)
-        var prev_data = mentors.filter(ele => ele.year != 2018)
+        if (data.success == true) {
+            var mentors = data.mentors
+            var this_data = mentors.filter(ele => ele.year == 2018)
+            var prev_data = mentors.filter(ele => ele.year != 2018)
 
-        insert_mentors(this_data, this_mentors)
-        insert_mentors(prev_data, prev_mentors)
+            insert_mentors(this_data, this_mentors)
+            insert_mentors(prev_data, prev_mentors)
 
-        if (this_data.length) this_div.style.display = 'block'
-        if (prev_data.length) prev_div.style.display = 'block'
+            if (this_data.length) this_div.style.display = 'block'
+            if (prev_data.length) prev_div.style.display = 'block'
 
-        // console.log(JSON.stringify(this_data, null, 2))
-        // console.log(JSON.stringify(prev_data, null, 2))
+            // console.log(JSON.stringify(this_data, null, 2))
+            // console.log(JSON.stringify(prev_data, null, 2))
 
-
+        } else {
+            this_div.style.display = 'block'
+            this_div.innerHTML += `<center><h3>Coming Soon</h3></center>`
+        }
     })
 
 
