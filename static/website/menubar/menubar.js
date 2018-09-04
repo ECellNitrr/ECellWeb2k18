133352b3c364701f97fde1abaaccc6c4261a3a1e
naveennvrgup <naveennvrgup@gmail.com>
2018-09-04 16:07:31 +0530

    menubar animation done

--- static/website/menubar/menubar.js
+++ static/website/menubar/menubar.js
@@ -1,28 +1,9 @@
 // these 2 are declared in the modal-dom js
 let menuTrigger = document.querySelector('.menu-trigger');
 let menubar = document.querySelector('.menubar');
+let menubar_a = document.querySelectorAll('.menubar a');
 
 menuTrigger.addEventListener('click', function () {
     menuTrigger.classList.toggle('menu-close');
     menubar.classList.toggle('menu-close');
-
-    // if(window.innerWidth<500) {
-    //     mobile_menu_toggle()
-    // }else {
-    //     desktop_menu_toggle()
-    // }
 });
-
-var mobile_menu_toggle = () => {
-    if (menubar.style.display == 'none' || !menubar.style.display) {
-        menubar.style.display = 'flex';
-        menuTrigger.classList.toggle('menu-close');
-    } else {
-        menubar.style.display = 'none';
-        menuTrigger.classList.remove('menu-close');
-    }
-}
-
-var desktop_menu_toggle = () => {
-    menuTrigger.classList.toggle('menu-close')
-}
