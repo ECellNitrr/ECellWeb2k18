f5e2863cb971eca74df4dfd3cb2fdee3784437ef
naveennvrgup <naveennvrgup@gmail.com>
2018-09-04 21:47:57 +0530

    added startups page and fixed some bugs in the index page and revamped the menubar

---
+++ static/website/startups/startups.js
@@ -0,0 +1,43 @@
+// show the gallery
+var gallery_div = document.querySelector('#gallery')
+for (var i = 0; i < 15; i++) {
+    gallery_div.innerHTML += `
+        <div class='gimg'>
+            <img src='/static/website/gallery/img/${i + 1}.jpg'>
+        </div>
+    `
+}
+
+// on click show big img
+var gimgs = document.querySelectorAll('.gimg img')
+var bimg = document.querySelector('#big_img')
+var bcontainer = document.querySelector('#big_container')
+var bcloseBtn = document.querySelector('#bcloseBtn')
+var leftBtn = document.querySelector('#left')
+var rightBtn = document.querySelector('#right')
+
+
+gimgs.forEach(gimg => {
+    gimg.addEventListener('click', function (e) {
+        e.preventDefault()
+        bimg.src = gimg.src
+        bcontainer.style.top = 0
+        bimg.style.height = '90vh'
+    })
+})
+
+
+bcontainer.addEventListener('click', (e) => {
+    e.preventDefault()
+    // if clicked on img return 
+    if (e.target != bcontainer) return
+    // if clicked on the container
+    bcontainer.style.top = "-100vh"
+    bimg.style.height = '0'
+})
+
+bcloseBtn.addEventListener('click', (e) => {
+    e.preventDefault()
+    bcontainer.style.top = "-100vh"
+    bimg.style.height = '0'
+})
