006b62b27d9b7ea8c2ab0c35d732f6d66784075d
naveennvrgup <naveennvrgup@gmail.com>
2018-09-05 19:16:42 +0530

    basic layout of the startup model

--- static/website/startups/startup_modal.js
+++ static/website/startups/startup_modal.js
@@ -5,6 +5,7 @@ var modal = document.querySelector('#modal')
 var modal_container = document.querySelector('#modal_container')
 var more_btns;
 
+// adding events listeners to more btns
 startup_modal = () => {
     more_btns = document.querySelectorAll('.more')
     more_btns.forEach(btn => {
@@ -13,24 +14,30 @@ startup_modal = () => {
             show_modal(btn.dataset.id)
         })
     })
+
+    show_modal(1)
 }
 
 show_modal = (id) => {
-    console.log(id)
     var i = startups.find((startup) => startup.id == id)
 
     modal.innerHTML = `
-    <div id=in_modal>
-        <div class=mimg-holder>
+    <div id='in_modal' class='row'>
+        <div class='mimg-holder col-md-5'>
             <img src=${i.pic}>
+            <div class='text-center'>
+                <button class=register>Register</button>
+                <button class=mclose>Close</button>
+            </div>
         </div>
-        <h3 class=title>${i.name}</h3>
-        <p>Founder: ${i.founder}</p>
-        <p>Contact: ${i.contact}</p>
-        <p>Email: ${i.email}</p>
-        <p>Address: ${i.address}</p>
-        <p class=desc>${i.details}</p>
-        <button class=register>register</button>
+        <div class='col-md-7'>
+            <h3 class=title>${i.name}</h3>
+            <p>Founder: ${i.founder}</p>
+            <p>Contact: ${i.contact}</p>
+            <p>Email: ${i.email}</p>
+            <p>Address: ${i.address}</p>
+            <p class=desc>${i.details}</p>
+        </div>
     </div>
     `
     // add the show class
