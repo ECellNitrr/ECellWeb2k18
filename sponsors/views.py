4f5bd7bcef334d1f5791b7519aec0bc5514645bd
naveensundar <naveen@matelabs.in>
2019-06-07 03:25:06 +0530

    turn on flag in spons

--- sponsors/views.py
+++ sponsors/views.py
@@ -14,22 +14,22 @@ def get_sponsors(request):
 	response = {}
 	sponsors = Sponsor.objects.all().values()
 	scheme = urlsplit(request.build_absolute_uri(None)).scheme
-	if Sponsor.objects.all().exists():
+	if Sponsor.objects.all(flag=True).exists():
 		response['success'] = True
-		AS = Sponsor.objects.filter(spons_type='AS').values()
+		AS = Sponsor.objects.filter(spons_type='AS',flag=True).values()
 		for spons in AS:
 			# sponsor['pic'] = scheme+'://'+request.META['HTTP_HOST']+'/'+str(sponsor['pic'])
 			spons['pic'] = config('HOST')+str(spons['pic'])
-		PLTS =Sponsor.objects.filter(spons_type='PLTS').values()
+		PLTS =Sponsor.objects.filter(spons_type='PLTS',flag=True).values()
 		for spons in PLTS:
 			spons['pic'] = config('HOST')+str(spons['pic'])
-		GS = Sponsor.objects.filter(spons_type='GS').values()
+		GS = Sponsor.objects.filter(spons_type='GS',flag=True).values()
 		for spons in GS:
 			spons['pic'] = config('HOST')+str(spons['pic'])
-		TS = Sponsor.objects.filter(spons_type='TS').values()
+		TS = Sponsor.objects.filter(spons_type='TS',flag=True).values()
 		for spons in TS:
 			spons['pic'] = config('HOST')+str(spons['pic'])
-		PRTS = Sponsor.objects.filter(spons_type='PRTS').values()
+		PRTS = Sponsor.objects.filter(spons_type='PRTS',flag=True).values()
 		for spons in PRTS:
 			spons['pic'] = config('HOST')+str(spons['pic'])
 		AS_list = list(AS)
