957fc1d66d4cd80be1d33c28fc244067351af869
naveensundar <naveen@matelabs.in>
2019-06-06 12:39:23 +0530

    yearwise list of sponsors added

--- sponsors/urls.py
+++ sponsors/urls.py
@@ -3,7 +3,9 @@ from django.urls import path
 from . import views
 urlpatterns = [
     url(r'^list/', views.get_sponsors),
-    url(r'^$', views.post_sponsors, name='Post_Sponsors')
+    path('<int:year>/', views.post_sponsors, name='Post_Sponsors'),
+    path('yearwise/', views.post_sponsors_yearwise_list, name='sponsors_yearwise')
+    # url(r'^$', views.post_sponsors, name='Post_Sponsors')
     #path('create/', views.create_spons),
     #path('view/<int:id>/', views.view),
     #path('delete/<int:id>/', views.delete_spons),
