58cfa44624feb004e8dd326f089d83e51843f0a1
deep-matelabs <deep-matelabs@github.com>
2019-06-13 23:37:21 +0530

    added redirect for sponsors/ route

--- sponsors/urls.py
+++ sponsors/urls.py
@@ -1,10 +1,13 @@
 from django.conf.urls import url, include
 from django.urls import path
+from django.views.generic import RedirectView
+
 from . import views
 urlpatterns = [
     url(r'^list/', views.get_sponsors),
     path('<int:year>/', views.post_sponsors, name='Post_Sponsors'),
-    path('yearwise/', views.post_sponsors_yearwise_list, name='sponsors_yearwise')
+    path('yearwise/', views.post_sponsors_yearwise_list, name='sponsors_yearwise'),
+    path('', RedirectView.as_view(url='/sponsors/2018/', permanent=True)),
     # url(r'^$', views.post_sponsors, name='Post_Sponsors')
     #path('create/', views.create_spons),
     #path('view/<int:id>/', views.view),
